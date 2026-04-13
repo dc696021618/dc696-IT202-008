5d39974d7855554943a62c812fee2afaae64878e
Cuadrado <cuadrado@cuadrados-air.mynetworksettings.com>
2026-04-13 06:29:33 -0400

    added new helper functions and conditional html to nav.php, also included some examples of using the helper functions

--- public_html/project/landing.php
+++ public_html/project/landing.php
@@ -4,12 +4,10 @@ require(__DIR__."/../../partials/nav.php");
 <h1>Landing Page</h1>
 <?php
 error_log("Session: ". var_export($_SESSION, true));
-if(isset($_SESSION["user"], $_SESSION["user"]["email"])){
- echo "Welcome, ";
- // Always escape user data before outputting it to prevent XSS, even if you trust your session data
- se($_SESSION["user"]["email"]);
-}
-else{
-  echo "You're not logged in";
+if (is_logged_in()) {
+    // Note, we don't need to use 'se()' here sinse 'get user_email()' uses it intertnally
+    echo "Welcome, " . get_user_email();
+} else {
+    echo "Your'e not logged in";
 }
 ?>
